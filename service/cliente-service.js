bcbef531ccaa77b055cbb95d52f7968bd99d40d0
Marcondes Amorim <marcondesjra@gmail.com>
2022-03-21 11:57:17 -0300

    preencher tabela de clientes

---
+++ service/cliente-service.js
@@ -0,0 +1,35 @@
+const criaNovaLinha = (nome, email) => { 
+
+    const linhaNovoClient = document.createElement('tr');
+
+    const conteudo = `
+        <td class="td" data-td>${nome}</td>
+            <td>${email}</td>
+            <td>
+                <ul class="tabela__botoes-controle">
+                    <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+                    <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+                </ul>
+            </td`;
+
+    linhaNovoClient.innerHTML = conteudo;
+    return linhaNovoClient;
+}
+
+const tabela = document.querySelector('[data-tabela]')
+
+const http = new XMLHttpRequest();
+http.open('GET', 'http://localhost:3000/profile');
+http.send(); 
+
+http.onload = () => {
+    const data = JSON.parse(http.response);
+    data.forEach(elemento => {
+        tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email));
+        
+    });
+
+    const http2 = new XMLHttpRequest();
+    
+    http2.open('GET', 'http://localhost:3000/profile');
+}
